ffe84658ad6cc06200218608fffbeb0d82631c17
Dhairya Sarvaiya <115853289+Byte717@users.noreply.github.com>
2024-07-14 11:15:50 -0400

    Update ai.js

--- UI/ai.js
+++ UI/ai.js
@@ -2,14 +2,14 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const fs = require("fs");
 
 
-const APIKEY = ""; // fil this in 
+const APIKEY = process.env.API_KEY; // add key to environment variable
 
 const getResponse = async () => {
     const genAI = new GoogleGenerativeAI(APIKEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
     const prompt = document.getElementById("prompt").value;
     if(prompt == "") return;
-    const result = await model.generateContent("Diagnose the disease and/or illness with the following symptoms:"+prompt); 
+    const result = await model.generateContent("Diagnose the disease and/or illness  and provide solutions with the following symptoms:"+prompt); 
     const response = await result.response;
     const text = response.text();
     document.getElementById("response").innerText = text;
